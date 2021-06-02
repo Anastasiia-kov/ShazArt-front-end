920d704530cbb2662d9950ed1b6166630d27b2fe
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-06-02 17:17:52 +0300

    smth

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,60 +1,60 @@
-import React, { useState } from 'react'
-import '../CSS/MainComponent.css'
+import React, { useState } from "react";
+import "../CSS/MainComponent.css";
 import Modal from "react-modal";
-import ArtistNameForm from './ArtistNameForm'
-import CameraUser from './CameraUser';
-import PainterPage from './PainterPage'
+import ArtistNameForm from "./ArtistNameForm";
+import CameraUser from "./CameraUser";
+import PainterPage from "./PainterPage";
 
 Modal.setAppElement("#root");
 const customStyles = {
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        background: "#fffff"
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    background: "#fffff",
+  },
 };
 
 function MainCmponent() {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [artistName, setArtistName] = useState(null)
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [artistName, setArtistName] = useState(null);
 
-    const closeModal = async () => {
-        setIsOpen(false);
-    };
+  const closeModal = async () => {
+    setIsOpen(false);
+  };
 
-    const openForm = async () => {
-        setIsOpen(true)
-    }
+  const openForm = async () => {
+    setIsOpen(true);
+  };
 
-    const handleArtistName = async (artist) => {
-        console.log(artist)
-        setArtistName(artist)
-        setIsOpen(false)
-    }
+  const handleArtistName = async (artist) => {
+    console.log(artist);
+    setArtistName(artist);
+    setIsOpen(false);
+  };
 
-    if(artistName) {
-        return <PainterPage artistName={artistName}/>
-    } else {
+  if (artistName) {
+    return <PainterPage artistName={artistName} />;
+  } else {
     return (
-        <div className="main-div">
-            <div className="app-name">ShazArt</div>
-            <button className="logo" onClick={openForm}></button>
-            <div className="text">Press to find the arts</div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-            >
-                <CameraUser artistName={handleArtistName}/>
-                {/* <ArtistNameForm></ArtistNameForm> */}
-            </Modal>
-        </div>
-    )
-    }
+      <div className="main-div">
+        <div className="app-name">ShazArt</div>
+        <button className="logo" onClick={openForm}></button>
+        <div className="text">Press to find the arts</div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+        >
+          <CameraUser artistName={handleArtistName} />
+          {/* <ArtistNameForm></ArtistNameForm> */}
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default MainCmponent
+export default MainCmponent;
