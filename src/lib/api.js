09ae33f78f5d2351f61faa2db98ae5d819a5eb24
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-06-02 17:02:23 +0300

    added api functions

---
+++ src/lib/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const url = "https://www.artic.edu";
+
+export async function getArtistId(input) {
+  const response = await axios.get(
+    `http://172.16.1.72:5000/artist?name=${input}`
+  );
+  return response.data;
+}
+
+export async function getArtistsCreations(artistId) {
+  const response = await axios.get(`${url}/api/v1/agents/${artistId}`);
+  return response.data;
+}
+
+export async function getArtworkById(artId) {
+  const response = await axios.get(
+    `${url}/api/v1/artworks/${artId}?fields=id,title,image_id`
+  );
+  return response.data;
+}
+
+export async function getEstimatePrice(width, height) {
+  const response = await axios.get(url + `${(width, height)}`);
+  return response.data;
+}
+
+export async function getPictureUrl(picId) {
+  const response = await axios.get(
+    `${url}/iiif/2/${picId}/full/843,/0/default.jpg`
+  );
+  return response.data;
+}
